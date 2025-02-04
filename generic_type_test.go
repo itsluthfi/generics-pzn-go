378b24bfd503f8af45fddfb979069f4ce28067d9
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2025-02-04 19:48:15 +0700

    Generic Type

---
+++ generic_type_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Bag[T any] []T
+
+func PrintBag[T any](bag Bag[T]) {
+	for _, value := range bag {
+		fmt.Println(value)
+	}
+}
+
+func TestBagString(t *testing.T) {
+	names := Bag[string]{"Luthfi", "Izzuddin", "Hanif"}
+	PrintBag(names)
+}
+
+func TestBagInt(t *testing.T) {
+	numbers := Bag[int]{1, 2, 3, 4, 5}
+	PrintBag[int](numbers)
+}
